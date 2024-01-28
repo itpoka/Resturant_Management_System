67f75d2a94c025962930c1cf599d01ae287bb61d
unknown <mustafizurrahman.nu.isdb@gmail.com>
2024-01-29 01:03:01 +0600

    My first Commit

--- online/admin.py
+++ online/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
+
 
 # Register your models here.
+class deliveryAdmin(admin.ModelAdmin):
+    list_display=['code','name','status','created_by','created_at']
+    class Meta:
+        model=Delivery
+
+admin.site.register(Delivery,deliveryAdmin)
--- online/models.py
+++ online/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_userforeignkey.models.fields import UserForeignKey
 
 # Create your models here.
+class Delivery(models.Model):
+    code = models.CharField(max_length=20,blank=True,null=True) 
+    name = models.CharField(max_length=100,blank=True,null=True)
+    status = models.BooleanField(default=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = UserForeignKey(auto_user_add=True,editable=False,related_name='delivery_creator')
+    updated_by = UserForeignKey(auto_user=True,editable=False,related_name='delivery_updator')
+
+    class Meta:
+        db_table='ac_delivery'
+
+    def _str_(self):
+        return self.name
